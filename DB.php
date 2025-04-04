5172df35dddace6c79fde284d960ded6aa8f4ef8
manraf11 <168047318+manraf11@users.noreply.github.com>
2025-04-04 11:51:22 -0400

    Delete DB.php

--- DB.php
+++
@@ -1,7 +0,0 @@
-<?php   
-// DB.php   
-$db_conn = pg_connect("host=localhost dbname=SIVI user=postgres password=123");   
-if (!$db_conn) {   
-    die("Error en la conexión a la base de datos.");   
-}   
-?>
